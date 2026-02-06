8e8d6d3ad11dae4c6689695299726e1dfe88854e
Zenkaku Hiragana <daiko1997@yahoo.co.jp>
2026-02-07 01:40:09 +0900

    Reworked the drawink shader

--- shaders/src/drawink_ps30.hlsl
+++ shaders/src/drawink_ps30.hlsl
@@ -1,38 +1,188 @@
 
-sampler Albedo   : register(s0); // $basetexture
-sampler Normal   : register(s1); // $texture1
-sampler Material : register(s2); // $texture2
-sampler Mask     : register(s3); // $texture3
+sampler InkMap             : register(s0);
+sampler DataSampler        : register(s1);
+sampler BaseTextureAtlas   : register(s2);
+sampler TintTextureAtlas   : register(s3);
+const float2 RcpRTSize     : register(c4); // One over render target size
+const float2 RcpDataRTSize : register(c5);
+
+static const float  eps = 1e-4;
+
+// [0.0, 1.0] --> [-1.0, +1.0]
+#define TO_SIGNED(x) ((x) * 2.0 - 1.0)
+
+// [-1.0, +1.0] --> [0.0, 1.0]
+#define TO_UNSIGNED(x) ((x) * 0.5 + 0.5)
+
 struct PS_INPUT {
-    float2 pos : VPOS;
-    float2 uv  : TEXCOORD0;
-};
-struct PS_OUTPUT {
-    float4 additive       : COLOR0;
-    float4 multiplicative : COLOR1;
-    float4 pbr            : COLOR2;
-    float4 details        : COLOR3;
+    float2 screenPos             : VPOS;
+    float4 inkUV_shapeUV         : TEXCOORD0;
+    float4 surfaceClipRange      : TEXCOORD1;
+    float4 timeRegionTypeOpacity : TEXCOORD2;
 };
 
-#define AddRGB     output.additive.rgb
-#define MulRGB     output.multiplicative.rgb
-#define Height     output.additive.a
-#define Refraction output.multiplicative.a
-#define PBR        output.pbr.rgb
-#define BumpBlend  output.pbr.a
-PS_OUTPUT main(const PS_INPUT i) {
-    PS_OUTPUT output;
-    float mask = tex2D(Mask, i.uv).a;
-    if (mask == 0) discard;
-    float4 albedo = tex2D(Albedo, i.uv);
-    float4 normal = tex2D(Normal, i.uv);
-    float4 pbr    = tex2D(Material, i.uv);
-    AddRGB = albedo.rgb;
-    MulRGB = float3(0.0, 0.0, 0.0);
-    Height = normal.z;
-    Refraction = 0.0;
-    PBR    = pbr.rgb;
-    BumpBlend = 0.75;
-    output.details = float4(0.0, 0.0, 0.0, 1.0);
+struct PS_OUTPUT {
+    float4 color : COLOR0;
+    float  depth : DEPTH0;
+};
+
+float4 FetchDataPixel(int id, int index) {
+    float4 uv = { id * RcpDataRTSize.x, index * RcpDataRTSize.y, 0.0, 0.0 };
+    return tex2Dlod(DataSampler, uv);
+}
+
+PS_OUTPUT AdditiveAndHeight(const PS_INPUT i, float t, float shapeMask) {
+    int paintType = int(i.timeRegionTypeOpacity.z);
+    float4 add = tex2D(BaseTextureAtlas, i.inkUV_shapeUV.xy);
+    float4 tint = tex2D(TintTextureAtlas, i.inkUV_shapeUV.xy);
+    float4 old = tex2D(InkMap, i.screenPos * RcpRTSize);
+    float4 colorAlphaParam = FetchDataPixel(paintType, 0);
+    float4 tintParam = FetchDataPixel(paintType, 1);
+    float4 heightParam = FetchDataPixel(paintType, 2);
+    float4 miscParam = FetchDataPixel(paintType, 4);
+
+    // Height map blend parameters
+    float nodig = step(0.5, miscParam.w);
+    float maxHeight = heightParam.x;
+    float meanHeight = heightParam.y;
+    float heightScale = TO_SIGNED(heightParam.z) * 2.0;
+
+    // Height map blend calculation
+    float paintHeight = add.a * heightScale; // -2.0 -- +2.0
+    float paintDetail = paintHeight - meanHeight * heightScale;
+    float oldHeight = TO_SIGNED(old.a);
+    float oldSpace = saturate(1.0 - sign(paintHeight) * oldHeight);
+    float baselineFalloff = exp(-sign(paintHeight) * oldHeight / maxHeight);
+    float baselineAdd = paintHeight * oldSpace * baselineFalloff;
+    float detailFalloff = pow(oldSpace, 0.2);
+    float detailAdd = paintDetail * detailFalloff;
+    float desiredAdd = baselineAdd + detailAdd;
+    float newHeight = oldHeight + desiredAdd;
+
+    // Digging
+    float paintStrength = 1.0;
+    if (desiredAdd < 0.0) {
+        float4 oldIndexUV = float4(i.screenPos * RcpRTSize + float2(0.0, 0.5), 0.0, 0.0);
+        float4 oldDepthUV = float4(i.screenPos * RcpRTSize + float2(0.5, 0.0), 0.0, 0.0);
+        float4 oldIndexSample = tex2Dlod(InkMap, oldIndexUV);
+        float oldDepth = tex2Dlod(InkMap, oldDepthUV).a;
+        float oldIndex = lerp(oldIndexSample.r, oldIndexSample.g, ceil(oldIndexSample.b));
+        float oldThickness = max(0.0, oldHeight + oldDepth);
+        float viscosity = FetchDataPixel(int(oldIndex), 4).z;
+        float fluidDigAmount = min(-desiredAdd * viscosity, oldThickness);
+        float solidDigAmount = -desiredAdd - fluidDigAmount;
+        float geometryPaintBias = tintParam.w;
+        newHeight = oldHeight - fluidDigAmount - solidDigAmount * viscosity;
+        paintStrength = saturate(1 - nodig - geometryPaintBias * step(-newHeight, oldDepth + eps));
+    }
+
+    // Basic tint by $color, $tintcolor, and $alpha
+    add.rgb *= colorAlphaParam.rgb * paintStrength;
+    tint.rgb *= colorAlphaParam.aaa * tintParam.rgb * paintStrength;
+    float erase = miscParam.x;
+    float flatten = miscParam.y;
+    float3 newColor = old.rgb * tint.rgb + add.rgb * (1.0 - erase);
+    float clampedHeight = max(nodig - 1.0, lerp(newHeight, 0.0, flatten));
+    PS_OUTPUT output = { newColor.r, newColor.g, newColor.b, TO_UNSIGNED(clampedHeight), t };
     return output;
+}
+
+PS_OUTPUT TintAndDepth(const PS_INPUT i, float t, float shapeMask) {
+    int paintType = int(i.timeRegionTypeOpacity.z);
+    float4 add = tex2D(BaseTextureAtlas, i.inkUV_shapeUV.xy);
+    float4 tint = tex2D(TintTextureAtlas, i.inkUV_shapeUV.xy);
+    float4 old = tex2D(InkMap, i.screenPos * RcpRTSize);
+    float4 colorAlphaParam = FetchDataPixel(paintType, 0);
+    float4 tintParam = FetchDataPixel(paintType, 1);
+    float4 heightParam = FetchDataPixel(paintType, 2);
+    float4 miscParam = FetchDataPixel(paintType, 4);
+    float nodig = step(0.5, miscParam.w);
+    float depthScale = heightParam.z;
+
+    // Digging
+    float depth = old.a;
+    float paintStrength = 1.0;
+    if (depthScale < 0.0 && nodig == 0.0) {
+        // Height map blend calculation
+        float flatten = miscParam.y;
+        float maxDepth = heightParam.x;
+        float meanDepth = heightParam.y;
+        float paintDepth = add.a * depthScale; // -2.0 -- 0.0
+        float paintDetail = paintDepth - meanDepth * depthScale;
+        float oldSpace = saturate(1.0 - depth);
+        float baselineFalloff = exp(-depth / maxDepth);
+        float baselineAdd = paintDepth * oldSpace * baselineFalloff; // < 0
+        float detailFalloff = pow(oldSpace, 0.2);
+        float detailAdd = paintDetail * detailFalloff;
+        float desiredAdd = baselineAdd + detailAdd; // < 0
+
+        // Applying viscosity of the fluid on top of the ground
+        static const float eps = 1e-4;
+        float4 oldIndexUV = float4(i.screenPos * RcpRTSize + float2(-0.5, 0.5), 0.0, 0.0);
+        float4 oldHeightUV = float4(i.screenPos * RcpRTSize + float2(0.5, 0.0), 0.0, 0.0);
+        float4 oldIndexSample = tex2Dlod(InkMap, oldIndexUV);
+        float oldHeight = tex2Dlod(InkMap, oldHeightUV).a;
+        float oldIndex = lerp(oldIndexSample.r, oldIndexSample.g, ceil(oldIndexSample.b));
+        float oldThickness = max(0.0, oldHeight + depth);
+        float viscosity = FetchDataPixel(int(oldIndex), 4).z;
+        float fluidDigAmount = min(-desiredAdd * viscosity, oldThickness);
+        float solidDigAmount = -desiredAdd - fluidDigAmount;
+        float geometryPaintBias = tintParam.w;
+        float newDepth = depth + solidDigAmount;
+        paintStrength = saturate(1 - geometryPaintBias * step(newDepth, depth + eps));
+        depth = max(-1.0, lerp(newDepth, 0.0, flatten));
+    }
+
+    // Do not darken more than maxLayers times
+    // But if current one is already darker than that, keep it
+    tint.rgb *= tintParam.rgb * colorAlphaParam.aaa * paintStrength;
+    float erase = miscParam.x;
+    float maxLayers = heightParam.w * 255;
+    float3 tintClipped = max(old.rgb * tint.rgb, pow(saturate(tint.rgb), maxLayers));
+    float3 tintFinal = lerp(min(tintClipped, old.rgb), 1.0, erase);
+    PS_OUTPUT output = { tintFinal.r, tintFinal.g, tintFinal.b, depth, depth };
+    return output;
+}
+
+PS_OUTPUT PaintIndices(const PS_INPUT i, float t, float shapeMask) {
+    float4 old = tex2D(InkMap, i.screenPos * RcpRTSize);
+    float inkType = i.timeRegionTypeOpacity.z;
+    float opacity = i.timeRegionTypeOpacity.w;
+    PS_OUTPUT output = {
+        opacity > 1.0 - eps ? inkType : old.r,
+        opacity < 1.0       ? inkType : old.g,
+        opacity < 1.0       ? opacity : 0.0,
+        0.0, t,
+    };
+    return output;
+}
+
+PS_OUTPUT DetailMapping(const PS_INPUT i, float t, float shapeMask) {
+    float detailRotation = 0.0;
+    float detailScale = 1.0;
+    PS_OUTPUT output = {
+        i.inkUV_shapeUV.x,
+        i.inkUV_shapeUV.y,
+        detailRotation,
+        detailScale, t,
+    };
+    return output;
+}
+
+PS_OUTPUT main(const PS_INPUT i) {
+    clip(i.screenPos * RcpRTSize - i.surfaceClipRange.xy);
+    clip(i.surfaceClipRange.zw - i.screenPos * RcpRTSize);
+
+    float4 shapeMask = tex2D(TintTextureAtlas, i.inkUV_shapeUV.zw);
+    clip(shapeMask.a == 0.0 ? -1.0 : 1.0);
+
+    float t = i.timeRegionTypeOpacity.x;
+    float regionIndex = i.timeRegionTypeOpacity.y;
+    if (regionIndex == 0)
+        return AdditiveAndHeight(i, t, shapeMask.a);
+    if (regionIndex == 1)
+        return TintAndDepth(i, t, shapeMask.a);
+    if (regionIndex == 2)
+        return PaintIndices(i, t, shapeMask.a);
+    return DetailMapping(i, t, shapeMask.a);
 }
--- shaders/src/drawink_vs30.hlsl
+++ shaders/src/drawink_vs30.hlsl
@@ -1,16 +1,102 @@
 
+// POSITION0
+//   xyz : The world position of the splash
+//   w   : Paint type ID
+// TEXCOORD0
+//   xyz : The world angle of the splash (x, y, z = pitch, yaw roll)
+//   w   : Opacity for this paint type (0 -> transparent, 1 -> fully opaque)
+// TEXCOORD1
+//   x : Ink texture atlas min U
+//   y : Ink texture atlas min V
+//   z : Ink texture atlas max U
+//   w : Ink texture atlas max V
+// TEXCOORD2
+//   x : Ink shape mask atlas min U
+//   y : Ink shape mask atlas min V
+//   z : Ink shape mask atlas max U
+//   w : Ink shape mask atlas max V
+// TEXCOORD3
+//   x : surface min U
+//   y : surface min V
+//   z : surface max U
+//   w : surface max V
+// TEXCOORD4
+//   x : width
+//   y : height
+//   z : region index (0, 1, 2, 3)
+//   w : corner index (0, 1, 2, 3)
+// TEXCOORD5
+//   x : Earliest time in batch
+//   y : Latest time in batch
+//   z : Time of this splash
+//   w : Unused
+// TEXCOORD6 -- TEXCOORD7 WorldToUV matrix
+
+// HammerUnitsToPixels goes to pixel shader constant
+
+static const float  CornerSignX[4] = { -1,  1, 1, -1 };
+static const float  CornerSignY[4] = { -1, -1, 1,  1 };
+static const float2 ConstantUV[4]  = {{ 0, 0 }, { 0, 1 }, { 1, 1 }, { 1, 0 }};
+static const float2 RegionOffset[4] = {
+    { 0.0, 0.0 }, { 0.0, 0.5 }, { 0.5, 0.0 }, { 0.5, 0.5 }
+};
+
 struct VS_INPUT {
-    float4 pos : POSITION0;
-    float2 uv  : TEXCOORD0;
+    float4   worldPosPaintType        : POSITION0;
+    float4   worldAnglesOpacity       : TEXCOORD0;
+    float4   inkTextureAtlasRange     : TEXCOORD1;
+    float4   shapeMaskAtlasRange      : TEXCOORD2;
+    float4   surfaceClipRange         : TEXCOORD3;
+    float4   sizeAndRegionCornerIndex : TEXCOORD4;
+    float4   timesAndPaintType        : TEXCOORD5;
+    float4x2 worldToUV                : TEXCOORD6;
 };
+
 struct VS_OUTPUT {
-    float4 pos : POSITION;
-    float2 uv  : TEXCOORD0;
+    float4 pos                   : POSITION;
+    float4 inkUV_shapeUV         : TEXCOORD0;
+    float4 surfaceClipRange      : TEXCOORD1;
+    float4 timeRegionTypeOpacity : TEXCOORD2;
 };
-const float4x4 cModelViewProj : register(c4);
+
+#define curTime v.timesAndPaintType.x
+#define maxTime v.timesAndPaintType.y
+#define minTime v.timesAndPaintType.z
 VS_OUTPUT main(const VS_INPUT v) {
+    float3 sine, cosine;
+    sincos(radians(v.worldAnglesOpacity.xyz), sine, cosine);
+    float3 worldForward = { cosine.x * cosine.y, cosine.x * sine.y, -sine.x };
+    float2 basisU = normalize(mul(float4(worldForward, 0.0), v.worldToUV));
+    float2 basisV = { -basisU.y, basisU.x };
+
+    float timeFraction = saturate((curTime - minTime) / (maxTime - minTime));
+    float2 uvPos = mul(float4(v.worldPosPaintType.xyz, 1.0), v.worldToUV);
+    float2 size = v.sizeAndRegionCornerIndex.xy;
+    int cornerIndex = int(v.sizeAndRegionCornerIndex.w);
+    int regionIndex = int(v.sizeAndRegionCornerIndex.z);
+    uvPos += basisU * size.x * CornerSignX[cornerIndex];
+    uvPos += basisV * size.y * CornerSignY[cornerIndex];
+
+    // Convert position to local to specific region
+    uvPos *= 0.5;
+    uvPos += RegionOffset[regionIndex];
+
     VS_OUTPUT output;
-    output.pos = mul(float4(v.pos.xyz, 1.0), cModelViewProj);
-    output.uv = v.uv;
+    float2 inkTextureMinUV = v.inkTextureAtlasRange.xy;
+    float2 inkTextureMaxUV = v.inkTextureAtlasRange.zw;
+    float2 shapeMaskMinUV = v.shapeMaskAtlasRange.xy;
+    float2 shapeMaskMaxUV = v.shapeMaskAtlasRange.zw;
+    output.pos = float4(uvPos * 2.0 - 1.0, timeFraction, 1.0);
+    output.pos.y *= -1.0;
+    output.inkUV_shapeUV = float4(
+        lerp(inkTextureMinUV, inkTextureMaxUV, ConstantUV[cornerIndex]),
+        lerp(shapeMaskMinUV, shapeMaskMaxUV, ConstantUV[cornerIndex]));
+    output.surfaceClipRange
+        = v.surfaceClipRange * 0.5 + RegionOffset[regionIndex].xyxy;
+    output.timeRegionTypeOpacity = float4(
+        timeFraction,
+        regionIndex,
+        v.worldPosPaintType.w,
+        v.worldAnglesOpacity.w);
     return output;
 }
